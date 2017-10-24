0e4d16e736fe80aa2bccc4da9f296508fc48e5eb
Oli <pohloliver@live.de>
2017-10-25 00:12:17 +0200

    code cleanup to comply with airnbn code style

--- src/digilib-iiif-migrate.js
+++ src/digilib-iiif-migrate.js
@@ -1,182 +1,173 @@
-module.exports = {
+const has = Object.prototype.hasOwnProperty;
 
-    /**
-     * Get the full IIIF link of the image
-     * @param {string} digilibUrl - digilib URL that should be checked
-     */
-    getIiifFull : (digilibUrl) => {
-
-        let base = module.exports.getIiifScaler(digilibUrl);
-        let filepath = module.exports.convertFilePath(digilibUrl);
-        let fullParameters = "/full/full/0/default.jpg";
-
-        return base + filepath + fullParameters;
-
-    },
-
-    /**
-     * Get the IIIF link of the image with the same parameters as the old scaler URL
-     * @param {string} digilibUrl - digilib URL to transform
-     */
-    getIiifModified : (digilibUrl) => {
-
-        let base = module.exports.getIiifScaler(digilibUrl);
-        let filepath = module.exports.convertFilePath(digilibUrl);
-        let modifiedParameters = module.exports.convertParameters(digilibUrl)
-
-        return base + filepath + modifiedParameters;
-
-    },
-
-    /**
-     * Get scaler base
-     * @param {string} digilibUrl - digilib url to retrieve base from
-     */
-    getIiifScaler : (digilibUrl) => {
-
-        return digilibUrl.substr(0, digilibUrl.indexOf('Scaler') + 'Scaler'.length) + "/IIIF/";
-
-    },
-
-    /**
-     * Convert the filepath from the digilib URL
-     * @param {string} digilibUrl - digilib URL to convert file path from
-     */
-    convertFilePath : (digilibUrl) => {
-
-        let filepath = module.exports.extractParameters(digilibUrl).fn;
-
-        // Remove leading slashes
-        if (filepath[0] === "/") {
-            filepath = filepath.substr(1);
-        }
-
-        return filepath.replace(/\//g, "!");
-
-    },
-
-    /**
-     * Extract parameters from the provided digilib URL
-     * @param {string} digilibUrl - digilib URL to extract parameters from
-     */
-    extractParameters : (digilibUrl) => {
-
-        let parameters = {};
-
-        // Get last part of parameter string
-        let parameterString = digilibUrl.split("?")[1];
-
-        // Map parameters to key value pair
-        parameterString.split("&").forEach((parameter) => {
-            let pair = parameter.split("=");
-            parameters[pair[0]] = pair[1];
-        });
-
-        return parameters;
-
-    },
-
-    /**
-     * Convert scaler parameters to IIIF
-     * @param {string} digilibUrl - digilib URL to extract parameters from
-     */
-    convertParameters : (digilibUrl) => {
-
-
-        let region = module.exports.getIiifRegion(digilibUrl);
-        let size = "";
-        let rotation = "";
-
-
-    },
-
-    /**
-     * Convert the region parameters to IIIF
-     * @param {string} digilibUrl -
-     * @returns {string}
-     */
-    getIiifRegion : (digilibUrl) => {
-
-        let region = "full";
-        let parameters = module.exports.extractParameters(digilibUrl);
-
-        // If relative offset and size is given
-        if (parameters.hasOwnProperty("wx") && parameters.hasOwnProperty("wy") &&
-            parameters.hasOwnProperty("ww") && parameters.hasOwnProperty("wh")) {
-
-            let offsetX = (parseFloat(parameters.wx) * 100).toString();
-            let offsetY = (parseFloat(parameters.wy) * 100).toString();
-            let width = (parseFloat(parameters.ww) * 100).toString();
-            let height = (parseFloat(parameters.wh) * 100).toString();
-
-            return `pct:${offsetX},${offsetY},${width},${height}`;
-
-        }
-
-        return region;
-
-    },
-
-    /**
-     * Convert the size parameters to IIIF
-     * @param digilibUrl
-     * @returns {string}
-     */
-    getIiifSize : (digilibUrl) => {
-
-        let size = "full";
-        let parameters = module.exports.extractParameters(digilibUrl);
-
-        let scalingFactor = 1;
-
-        if (parameters.hasOwnProperty("ws")) {
-            scalingFactor = parameters.ws;
-        }
-
-        if (parameters.hasOwnProperty("dw") && parameters.has("dh")) {
-            let width = Math.round((parseInt(parameters.dw) * scalingFactor)).toString();
-            let height = Math.round((parseInt(parameters.dh) * scalingFactor)).toString();
-            return `${width},${height}`;
-        }
-
-        if (parameters.hasOwnProperty("dw")) {
-            let width = Math.round((parseInt(parameters.dw) * scalingFactor)).toString();
-            return `${width},`;
-        }
-
-        if (parameters.hasOwnProperty("dh")) {
-            let height = Math.round((parseInt(parameters.dh) * scalingFactor)).toString();
-            return `${height.toString()}`;
-        }
-
-        return size;
-
-    },
-
-    /**
-     * Check whether the provided url is a digilib scaler URL
-     * @param {string} digilibUrl - digilib URL that should be checked
-     */
-    isDigilibScaler : (digilibUrl) => {
-        return (digilibUrl.includes("Scaler"));
-    },
-
-    /**
-     * Check whether the provided url is an old fashioned digilib scaler
-     * @param {string} digilibUrl - digilib URL that should be checked
-     */
-    isDigilibOldScaler : (digilibUrl) => {
-        return (digilibUrl.includes("Scaler?fn="));
-    },
-
-
-    /**
-     * Check whether the the provided url is already IIIF compliant
-     * @param  {string} digilibUrl
-     */
-    isDigilibIiifScaler : (digilibUrl) => {
-        return (digilibUrl.includes("Scaler/IIIF/"));
-    }
-
+/**
+ * Get scaler base
+ * @param {string} digilibUrl - digilib url to retrieve base from
+ */
+function getIiifScaler(digilibUrl) {
+  return `${digilibUrl.substr(0, digilibUrl.indexOf('Scaler') + 'Scaler'.length)}/IIIF/`;
 }
+
+/**
+ * Extract parameters from the provided digilib URL
+ * @param {string} digilibUrl - digilib URL to extract parameters from
+ */
+function extractParameters(digilibUrl) {
+  const parameters = {};
+
+  // Get last part of parameter string
+  const parameterString = digilibUrl.split('?')[1];
+
+  // Map parameters to key value pair
+  parameterString.split('&').forEach((parameter) => {
+    const pair = parameter.split('=');
+
+    parameters[pair[0]] = pair[1];
+  });
+
+  return parameters;
+}
+
+/**
+ * Convert the filepath from the digilib URL
+ * @param {string} digilibUrl - digilib URL to convert file path from
+ */
+function convertFilePath(digilibUrl) {
+  let filepath = extractParameters(digilibUrl).fn;
+
+  // Remove leading slashes
+  if (filepath[0] === '/') {
+    filepath = filepath.substr(1);
+  }
+
+  return filepath.replace(/\//g, '!');
+}
+
+/**
+ * Get the full IIIF link of the image
+ * @param {string} digilibUrl - digilib URL that should be checked
+ */
+function getIiifFull(digilibUrl) {
+  const base = getIiifScaler(digilibUrl);
+  const filepath = convertFilePath(digilibUrl);
+  const fullParameters = '/full/full/0/default.jpg';
+
+  return base + filepath + fullParameters;
+}
+
+/**
+ * Convert the region parameters to IIIF
+ * @param {string} digilibUrl -
+ * @returns {string}
+ */
+function getIiifRegion(digilibUrl) {
+  const region = 'full';
+  const parameters = extractParameters(digilibUrl);
+
+  // If relative offset and size is given
+  if (has.call(parameters, 'wx') && has.call(parameters, 'wy') && has.call(parameters, 'ww') && has.call(parameters, 'wh')) {
+    const offsetX = (parseFloat(parameters.wx) * 100).toString();
+    const offsetY = (parseFloat(parameters.wy) * 100).toString();
+    const width = (parseFloat(parameters.ww) * 100).toString();
+    const height = (parseFloat(parameters.wh) * 100).toString();
+
+    return `pct:${offsetX},${offsetY},${width},${height}`;
+  }
+
+  return region;
+}
+
+/**
+ * Convert scaler parameters to IIIF
+ * @param {string} digilibUrl - digilib URL to extract parameters from
+ */
+function convertParameters(digilibUrl) {
+  const region = getIiifRegion(digilibUrl);
+  const size = '';
+  const rotation = '';
+}
+
+/**
+ * Get the IIIF link of the image with the same parameters as the old scaler URL
+ * @param {string} digilibUrl - digilib URL to transform
+ */
+function getIiifModified(digilibUrl) {
+  const base = getIiifScaler(digilibUrl);
+  const filepath = convertFilePath(digilibUrl);
+  const modifiedParameters = convertParameters(digilibUrl);
+
+  return base + filepath + modifiedParameters;
+}
+
+/**
+ * Convert the size parameters to IIIF
+ * @param digilibUrl
+ * @returns {string}
+ */
+function getIiifSize(digilibUrl) {
+  const size = 'full';
+  const parameters = extractParameters(digilibUrl);
+
+  let scalingFactor = 1;
+
+  if (has.call(parameters, 'ws')) {
+    scalingFactor = parameters.ws;
+  }
+
+  if (has.call(parameters, 'dw') && has.call(parameters, 'dh')) {
+    const width = Math.round((parseInt(parameters.dw, 10) * scalingFactor)).toString();
+    const height = Math.round((parseInt(parameters.dh, 10) * scalingFactor)).toString();
+    return `${width},${height}`;
+  }
+
+  if (has.call(parameters, 'dw')) {
+    const width = Math.round((parseInt(parameters.dw, 10) * scalingFactor)).toString();
+    return `${width},`;
+  }
+
+  if (has.call(parameters, 'dh')) {
+    const height = Math.round((parseInt(parameters.dh, 10) * scalingFactor)).toString();
+    return `${height.toString()}`;
+  }
+
+  return size;
+}
+
+/**
+ * Check whether the provided url is a digilib scaler URL
+ * @param {string} digilibUrl - digilib URL that should be checked
+ */
+function isDigilibScaler(digilibUrl) {
+  return digilibUrl.includes('Scaler');
+}
+
+/**
+ * Check whether the provided url is an old fashioned digilib scaler
+ * @param {string} digilibUrl - digilib URL that should be checked
+ */
+function isDigilibOldScaler(digilibUrl) {
+  return digilibUrl.includes('Scaler?fn=');
+}
+
+/**
+ * Check whether the the provided url is already IIIF compliant
+ * @param  {string} digilibUrl
+ */
+function isDigilibIiifScaler(digilibUrl) {
+  return digilibUrl.includes('Scaler/IIIF/');
+}
+
+export default {
+  getIiifFull,
+  getIiifModified,
+  getIiifScaler,
+  convertFilePath,
+  extractParameters,
+  convertParameters,
+  getIiifRegion,
+  getIiifSize,
+  isDigilibScaler,
+  isDigilibOldScaler,
+  isDigilibIiifScaler,
+};
 
--- tests/test-digilib-iiif-migrate.js
+++ tests/test-digilib-iiif-migrate.js
@@ -1,107 +1,88 @@
 import test from 'ava';
 import dim from '../src/digilib-iiif-migrate';
 
-let validOldUrl = "https://digilib.bbaw.de/digitallibrary/servlet/Scaler?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&dw=500&";
-let invalidOldUrl = "https://digilib.bbaw.de/digitallibrary/servlet/Faker?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&dw=500&";
+const validOldUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&dw=500&';
+const invalidOldUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Faker?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&dw=500&';
 
-let validIiifUrl = "https://digilib.bbaw.de/digitallibrary/servlet/Scaler/IIIF/silo10!Koran!Umwelttexte!Yazdgerd%20III.jpg/full/full/0/default.jpg";
-let invalidIiifUrl = "https://digilib.bbaw.de/digitallibrary/servlet/Scaler/Faker/silo10!Koran!Umwelttexte!Yazdgerd%20III.jpg/full/full/0/default.jpg";
+const validIiifUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler/IIIF/silo10!Koran!Umwelttexte!Yazdgerd%20III.jpg/full/full/0/default.jpg';
+const invalidIiifUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler/Faker/silo10!Koran!Umwelttexte!Yazdgerd%20III.jpg/full/full/0/default.jpg';
 
-test('it checks digilib urls for scalers', t => {
-
-    t.true(dim.isDigilibScaler(validOldUrl));
-    t.false(dim.isDigilibScaler(invalidOldUrl));
-    t.true(dim.isDigilibScaler(validIiifUrl));
-    t.true(dim.isDigilibScaler(invalidIiifUrl));
-
+test('it checks digilib urls for scalers', (t) => {
+  t.true(dim.isDigilibScaler(validOldUrl));
+  t.false(dim.isDigilibScaler(invalidOldUrl));
+  t.true(dim.isDigilibScaler(validIiifUrl));
+  t.true(dim.isDigilibScaler(invalidIiifUrl));
 });
 
-test('it checks digilib urls for old scalers', t => {
-
-    t.true(dim.isDigilibOldScaler(validOldUrl));
-    t.false(dim.isDigilibOldScaler(invalidOldUrl));
-    t.false(dim.isDigilibOldScaler(validIiifUrl));
-    t.false(dim.isDigilibOldScaler(invalidIiifUrl));
-
+test('it checks digilib urls for old scalers', (t) => {
+  t.true(dim.isDigilibOldScaler(validOldUrl));
+  t.false(dim.isDigilibOldScaler(invalidOldUrl));
+  t.false(dim.isDigilibOldScaler(validIiifUrl));
+  t.false(dim.isDigilibOldScaler(invalidIiifUrl));
 });
 
-test('it checks digilib urls for iiif scalers', t => {
-
-    t.false(dim.isDigilibIiifScaler(validOldUrl));
-    t.false(dim.isDigilibIiifScaler(invalidOldUrl));
-    t.true(dim.isDigilibIiifScaler(validIiifUrl));
-    t.false(dim.isDigilibIiifScaler(invalidIiifUrl));
-
+test('it checks digilib urls for iiif scalers', (t) => {
+  t.false(dim.isDigilibIiifScaler(validOldUrl));
+  t.false(dim.isDigilibIiifScaler(invalidOldUrl));
+  t.true(dim.isDigilibIiifScaler(validIiifUrl));
+  t.false(dim.isDigilibIiifScaler(invalidIiifUrl));
 });
 
-test('it retrieves the IIIF scaler base', t => {
-
-    let match = "https://digilib.bbaw.de/digitallibrary/servlet/Scaler/IIIF/";
-    t.is(match, dim.getIiifScaler(validOldUrl));
-
+test('it retrieves the IIIF scaler base', (t) => {
+  const match = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler/IIIF/';
+  t.is(match, dim.getIiifScaler(validOldUrl));
 });
 
-test('it converts the file path to the digilib IIIF format', t => {
-
-    let match = "silo10!Koran!Umwelttexte!Yazdgerd%20III.jpg";
-    t.is(match, dim.convertFilePath(validOldUrl));
-
+test('it converts the file path to the digilib IIIF format', (t) => {
+  const match = 'silo10!Koran!Umwelttexte!Yazdgerd%20III.jpg';
+  t.is(match, dim.convertFilePath(validOldUrl));
 });
 
-test('it converts the old scaler URL to IIIF', t => {
-
-    t.is(validIiifUrl, dim.getIiifFull(validOldUrl));
-
+test('it converts the old scaler URL to IIIF', (t) => {
+  t.is(validIiifUrl, dim.getIiifFull(validOldUrl));
 });
 
-test('it extracts parameters as an object', t => {
+test('it extracts parameters as an object', (t) => {
+  const targetFileName = '/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg';
+  const targetWidth = '500';
 
-    let targetFileName = "/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg";
-    let targetWidth = '500';
+  const parameters = dim.extractParameters(validOldUrl);
 
-    let parameters = dim.extractParameters(validOldUrl);
-
-    t.is(targetFileName, parameters.fn);
-    t.is(targetWidth, parameters.dw);
-
+  t.is(targetFileName, parameters.fn);
+  t.is(targetWidth, parameters.dw);
 });
 
-test('it converts the region parameters to full when no region parameters are specified at all', t => {
-
-    let scalerUrl = "https://digilib.bbaw.de/digitallibrary/servlet/Scaler?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&wx=0.1&wy=0.15&ww=0.43&wh=0.354";
-
-    let targetRegion = "pct:10,15,43,35.4";
-
-    t.is(targetRegion, dim.getIiifRegion(scalerUrl));
-
-
+test('it converts the region parameters to full when no region parameters are specified at all', (t) => {
+  const scalerUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg';
+  const targetRegion = 'full';
+  t.is(targetRegion, dim.getIiifRegion(scalerUrl));
 });
 
-test('it converts the region parameters to full when not all region parameters are specified', t => {
-
+test('it converts the region parameters to full when not all region parameters are specified', (t) => {
+  const scalerUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&wx=0.1&wy=0.15&ww=0.43';
+  const targetRegion = 'full';
+  t.is(targetRegion, dim.getIiifRegion(scalerUrl));
 });
 
 
-test('it converts the region parameters correctly when all region parameters are specified', t => {
-
+test('it converts the region parameters correctly when all region parameters are specified', (t) => {
+  const scalerUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&wx=0.1&wy=0.15&ww=0.43&wh=0.354';
+  const targetRegion = 'pct:10,15,43,35.4';
+  t.is(targetRegion, dim.getIiifRegion(scalerUrl));
 });
 
-test('it converts the size parameters to full when no size paramters are specified at all ', t => {
-
+test('it converts the size parameters to full when no size paramters are specified at all ', (t) => {
+  const scalerUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&wx=0.1&wy=0.15&ww=0.43&wh=0.354';
+  const targetSize = 'full';
+  t.is(targetSize, dim.getIiifSize(scalerUrl));
 });
 
-test('it converts the size parameters correctly when height and width are specified', t => {
-
+test('it converts the size parameters correctly when height and width are specified', (t) => {
+  const scalerUrl = 'https://digilib.bbaw.de/digitallibrary/servlet/Scaler?fn=/silo10/Koran/Umwelttexte/Yazdgerd%20III.jpg&wx=0.1&wy=0.15&ww=0.43&wh=0.354&dw=0.57&dh=0.47';
 });
 
-test('it converts the size parameters correctly when only height is specified', t => {
+test.todo('it converts the size parameters correctly when only height is specified');
 
-});
+test.todo('it converts the size parameters correctly when only width is specified');
 
-test('it converts the size parameters correctly when only width is specified', t => {
-
-});
-
-test('it applies the scaling factor to the size conversion correctly', t => {
-
-});
+test.todo('it applies the scaling factor to the size conversion correctly');
